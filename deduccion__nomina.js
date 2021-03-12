f3f982b1924e357571b610508dd702691c0a6cd1
Daniela <dacomu24@gmail.com>
2021-03-11 21:48:33 -0500

    Agregando objetos y metodos al proyecto

--- deduccion__nomina.js
+++ deduccion__nomina.js
@@ -1,10 +1,61 @@
 
 //Deducción de nomina - Calculos en base a la reglamentacion Colombiana
+// Creacion de empleados nuevos
 
-let salario = prompt("Por favor ingrese su salario en COP");
+class Empleado{ //Clase para crear empleado
+    constructor(nombres, apellidos,edad,salario){
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.edad = edad;
+        this.salario = salario;
+    }
+    deduccionSalud(){
+        this.salario = this.salario*0.04;
+    }
+    deduccionPension(){
+        this.salario = this.salario*0.04;
+    }
+    static prestacionesSociales(){
+        let cesantias = this.salario*0.01 ;
+        let prima = this.salario*0.08;
+        let vacaciones = this.salario*0.04; 
+    }    
+}
+
+let numeroEmpleados = parseInt(prompt("Ingrese la cantidad de empleados que desea crear: "));
+let listaEmpleados = {};
 
 
-function deduccion__salud (sueldo){
+function pedirDatosEmpleado(){
+    for (let i = 1; i <= numeroEmpleados;i++){
+        alert("Ingrese los datos del empleado " + i + " :")
+        let nombres = prompt("Ingrese nombres:");
+        let apellidos = prompt("Ingrese apellidos:");
+        let edad = parseInt(prompt("Ingrese edad:"));
+        let salario = parseInt(prompt("Ingrese salario:")); 
+        listaEmpleados[i] = new Empleado(nombres,apellidos,edad,salario); 
+    }
+    
+}
+if (numeroEmpleados > 0){
+    pedirDatosEmpleado();
+    alert("Revise los empleados agregados en la consola: ");
+} else{
+    alert("Regresa cuando quieras ingresar nuevos empleados");
+}
+
+console.log(listaEmpleados);
+
+/* function prestaciones__sociales (sueldo){
+    cesantias = sueldo*0.01 ;
+    prima = sueldo*0.08;
+    vacaciones = sueldo*0.04;
+    return {cesantias, prima, vacaciones};
+}
+ */
+
+
+/* function deduccion__salud (sueldo){
     deduccion = sueldo*0.04 ;
     deduccion__empresa = sueldo*0.125;
     return {deduccion, deduccion__empresa};
@@ -17,16 +68,10 @@ function deduccion__pension (sueldo){
     return {deduccion, deduccion__empresa};
     
 }
-
-function prestaciones__sociales (sueldo){
-    cesantias = sueldo*0.01 ;
-    prima = sueldo*0.08;
-    vacaciones = sueldo*0.04;
-    return {cesantias, prima, vacaciones};
-}
+ */
 
 
-let salud = deduccion__salud(salario);
+/* let salud = deduccion__salud(salario);
 let pension = deduccion__pension(salario);
 let prestaciones = prestaciones__sociales(salario);
 
@@ -34,4 +79,5 @@ console.log("Su deducción de sueldo en salud es: " + salud.deduccion + " y la e
 
 console.log("Su deducción de sueldo en pension es: " + pension.deduccion + " y la empresa aporta a su pensión: " + pension.deduccion__empresa);
 
-console.log("Su prestaciones sociales de este mes son: " + prestaciones.cesantias + " en cesantias," + prestaciones.prima + " en prima, y "+ prestaciones.vacaciones + " en vacaciones");
+console.log("Su prestaciones sociales de este mes son: " + prestaciones.cesantias + " en cesantias," + prestaciones.prima + " en prima, y "+ prestaciones.vacaciones + " en vacaciones"); */
+
